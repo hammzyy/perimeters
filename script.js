b264f8d48a4d8a27e7f4f47d89c5558601a65065
hammzyy <okenlaha@gmail.com>
2022-08-24 14:17:34 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// perimeter of triangle
+var a= 7 
+var b= 7
+var c= 14
+var pt= a + b + c
+
+console.log(pt)
+
+// perimeter of square
+var s= 6
+var ps= 4 * s
+
+console.log(ps)
+
+// perimeter of rectangle
+var l= 6
+var w= 6
+var pr= 2 * (l + w)
+
+console.log(pr)
+
+// perimeter of circle
+var pi= 22/7
+var r= 3
+var pc= 2 * (pi *r)
+
+console.log(pc)
+
+
